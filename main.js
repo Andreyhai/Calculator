07980911f249f9d0734c74a34133aeece76b9e70
Andrei <ustavitski.andrei@gmail.com>
2022-11-10 23:28:11 +0300

    last commit

--- main.js
+++ main.js
@@ -4,15 +4,15 @@ const f = (text) => {
 
 // объявление-----------------------------------------------------------------------------------------------------------
 
-let pole, del, ravenous, str, plus, x1;
+let pole, del, ravenous, str, plus, x1, flag, flag2, x2;
 
-pole = document.querySelector('.input');
+pole = document.querySelector('.input-text');
 del = document.querySelector('.delete');
 ravenous = document.querySelector('#br');
 plus = document.querySelector('#plus');
 str = '';
-x1 = 0;
-
+x1 = x2 = 0;
+flag = flag2 = false;
 // объявление-----------------------------------------------------------------------------------------------------------
 
 
@@ -41,7 +41,14 @@ del.addEventListener('click',
 ravenous.addEventListener('click',
     event => {
         event.preventDefault();
-        pole.innerText = '';
+
+        if (w) {
+            x2 = parseInt(str);
+            f(`x2 = ${x2}`);
+            x1 += x2;
+            f(`x1 + x2 = ${x1}`)
+            pole.innerText = x1;
+        }
 
     }
 );
@@ -55,24 +62,34 @@ ravenous.addEventListener('click',
 plus.addEventListener('click',
     event => {
         event.preventDefault();
-        f(str);
-        x1 = parseInt(str);
-        f(`x1 = ${x1}`);
-        pole.innerText = str = '';
+        if (w) {
+            pole.innerText = x1;
+        } else {
+            x1 = parseInt(str);
+            flag = true;
+            f(`x1 = ${x1}`);
+            pole.innerText = str = '';
+        }
     }
 );
 
 // плюс-----------------------------------------------------------------------------------------------------------------
 
-
+let w = false;
 const getValue = (selector) => {
     const mySelector = document.querySelector('#' + String(selector));
 
     mySelector.addEventListener('click',
         event => {
             event.preventDefault();
-            str += mySelector.value;
-            pole.innerText = str;
+            if (flag) {
+                str += mySelector.value;
+                pole.innerText = str;
+                w = true;
+            } else {
+                str += mySelector.value;
+                pole.innerText = str;
+            }
         }
     );
     return 0;
@@ -89,20 +106,3 @@ button6 = getValue('b6');
 button7 = getValue('b7');
 button8 = getValue('b8');
 button9 = getValue('b9');
-
-let theme = document.querySelector('.theme');
-let count = 0;
-
-theme.addEventListener('click',
-    event => {
-        event.preventDefault();
-        if (count = 0) {
-            document.querySelector("body").style.background = '#000000';
-            theme.src = 'icons/icons8-символ-луны-30.png'
-            count++;
-        } else {
-            theme.style.background = 'white';
-            count = 0;
-        }
-    }
-)
