9a6fb51ae799842aa2add93169ac02937ebfc0a1
Andrei <ustavitski.andrei@gmail.com>
2022-11-14 15:22:32 +0300

    last commit

--- main.js
+++ main.js
@@ -2,170 +2,65 @@ const f = (text) => {
   console.log(text);
 }
 
-// объявление-----------------------------------------------------------------------------------------------------------
+let pole, myStr, result, myrtle;
 
-let pole, ravenous, str, plus, myStr, minus, pro, sqrt, flag, del, theme;
-
+myStr = result ='';
 pole = document.querySelector('.input-text');
-ravenous = document.querySelector('#br');
-plus = document.querySelector('#plus');
-sqrt = document.querySelector('#sqrt');
-pro = document.querySelector('#pro');
-minus = document.querySelector('#minus');
-del = document.querySelector('.delete');
-theme = document.querySelectorAll('.theme');
-flag = false;
-str = myStr = '';
 
-// объявление-----------------------------------------------------------------------------------------------------------
+const numbers = document.querySelectorAll(".button");
 
+for (let i = 0; i < numbers.length; i++) {
+    let button = numbers[i];
+    button.addEventListener('click', event => {
+        event.preventDefault();
+        result += button.value;
+        pole.innerText = result;
+    });
+}
 
-
-
-
-
-
-// очистка--------------------------------------------------------------------------------------------------------------
-
-del.addEventListener('click',
-    () => {
-    pole.innerText = str = myStr = '';
-    }
-)
-
-// очистка--------------------------------------------------------------------------------------------------------------
-
-
-
-
-// равно----------------------------------------------------------------------------------------------------------------
-
+const actions = document.querySelectorAll('.any-button')
+for (let i = 0; i < actions.length; i++) {
+    if (myrtle[myrtle.length - 1] !== actions[i].value)
+        actions[i].addEventListener('click',
+            event => {
+                event.preventDefault();
+                myrtle = eval(result);
+                result = '';
+                pole.innerText = myrtle;
+            }
+        );
+}
+const ravenous = document.querySelector('#br');
 ravenous.addEventListener('click',
     event => {
         event.preventDefault();
-        if (flag) {
-            myStr += `${str})`;
-            f(myStr);
-            f(eval(myStr));
-            pole.innerText = eval(myStr);
-            str = myStr = '';
-        } else {
-            f(`str = ${str}`);
-            myStr += str;
-            f(`myStr = ${myStr}`)
-            pole.innerText = eval(myStr);
-            str = myStr = '';
-        }
+        myrtle = eval(result);
+        result = '';
+        pole.innerText = myrtle;
     }
 );
 
-// равно----------------------------------------------------------------------------------------------------------------
-
-
-
-// плюс-----------------------------------------------------------------------------------------------------------------
-
-plus.addEventListener('click',
-    event => {
-        event.preventDefault();
-        f(`str = ${str}`);
-
-        myStr += `${str}+`;
-        f(`myStr = ${myStr}`);
-        pole.innerText = str = '';
-    }
-);
-
-// плюс-----------------------------------------------------------------------------------------------------------------
-
-// минус----------------------------------------------------------------------------------------------------------------
-
-minus.addEventListener('click',
-    event => {
-        event.preventDefault();
-        f(`str = ${str}`);
-
-        myStr += `${str}-`;
-        f(`myStr = ${myStr}`);
-        pole.innerText = str = '';
-    }
-);
-
-// минус----------------------------------------------------------------------------------------------------------------
-
-// корень---------------------------------------------------------------------------------------------------------------
-
-sqrt.addEventListener('click',
-    event => {
-        event.preventDefault();
-        f(`str = ${str}`);
-        pole.innerText = sqrt.value;
-        myStr += `Math.sqrt(`;
-        flag = true;
-        f(flag);
-        f(`myStr = ${myStr}`);
-        pole.innerText = str = '';
-    }
-);
-
-// корень---------------------------------------------------------------------------------------------------------------
-
-// произведение---------------------------------------------------------------------------------------------------------
-
-pro.addEventListener('click',
-    event => {
-        event.preventDefault();
-        f(`str = ${str}`);
-
-        myStr += `${str}*`;
-        f(`myStr = ${myStr}`);
-        pole.innerText = str = '';
-    }
-);
-
-// произведение---------------------------------------------------------------------------------------------------------
-
-// тема-----------------------------------------------------------------------------------------------------------------
-
-
-const body = document.querySelector('body');
-document.querySelector('.theme1').addEventListener('click',
-    event => {
-        event.preventDefault();
-        body.cssText = 'background: aqua';
-        alert(0)
-    }
-)
-document.querySelector('.theme2').addEventListener('click',
-    event => {
-        event.preventDefault();
-        body.cssText = 'background: white';
-        alert(1)
-    }
-)
-const getValue = (selector) => {
-    const mySelector = document.querySelector('#' + String(selector));
-
-    mySelector.addEventListener('click',
-        event => {
-            event.preventDefault();
-            str += mySelector.value;
-            pole.innerText = str;
+changeTheme();
+function changeTheme () {
+    let myLine, theme;
+    myLine = true;
+    theme = document.querySelectorAll('.theme');
+    theme.forEach((item) => {
+            item.addEventListener('click',
+                event => {
+                    event.preventDefault();
+                    if (myLine && item.innerText === 'тёмная') {
+                        document.querySelector('body').classList.add('dark');
+                        myLine = false;
+                    } else if (myLine === false && item.innerText === 'светлая') {
+                        document.querySelector('body').classList.remove('dark');
+                        myLine = true;
+                    }
+                }
+            );
         }
     );
     return 0;
 }
-
-let button1,button2,button3,button4,button5,button6,button7,button8,button9;
-
-button1 = getValue('b1');
-button2 = getValue('b2');
-button3 = getValue('b3');
-button4 = getValue('b4');
-button5 = getValue('b5');
-button6 = getValue('b6');
-button7 = getValue('b7');
-button8 = getValue('b8');
-button9 = getValue('b9');
 
 f('done');
